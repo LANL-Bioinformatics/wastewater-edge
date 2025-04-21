5ef4842791eb4e7a250a807ee0b15f5f415e6aa9
Chienchi Lo <chien-chi@pn2034281.lanl.gov>
2025-04-21 12:58:42 -0600

    add tooltips for binning

--- webapp/client/src/workflows/metagenomics/defaults.js
+++ webapp/client/src/workflows/metagenomics/defaults.js
@@ -823,12 +823,14 @@ export const workflows = {
     rawReadsInput: {
       source: 'fasta',
       text: 'CONTIGS/FASTA',
+      tooltip:
+        'The binning program in EDGE is MaxBin2. MaxBin2 is a tool designed for metagenomic binning, where it clusters assembled contigs from metagenomes into genome bins based on sequence composition and abundance information. ',
       fasta: {
         enableInput: true,
         placeholder: 'Select a file or enter a file http(s) url',
         dataSources: ['upload', 'public', 'project'],
         fileTypes: ['fasta', 'fa', 'fna', 'contigs'],
-        projectTypes: ['assembly'],
+        projectTypes: ['assembly','annotation'],
         projectScope: ['self+shared'],
         viewFile: false,
         isOptional: false,
@@ -850,11 +852,14 @@ export const workflows = {
       binningMaxItr: {
         text: 'Maximum EM Algorithm Iteration',
         value: 50,
+        tooltip:
+          'It limits how many times MaxBin2 runs the EM refinement process. 50 is a balance between performance and quality of binning. Users can change it if you think your data needs more or fewer iterations to reach a good convergence (e.g., if you\'re using very complex or very simple datasets).',
         rangeInput: { defaultValue: 50, min: 1, max: 100, step: 1 },
       },
       binningProb: {
         text: 'EM Probability',
-        tooltip: 'Probability threshold for EM final classification.',
+        tooltip:
+          'It\'s the confidence cutoff for assigning contigs to bins. 90% ensures high-confidence assignments. A lower threshold would increase bin completeness but may reduce purity, while a higher threshold increases purity but may miss borderline contigs.',
         value: 0.9,
         rangeInput: { defaultValue: 0.9, min: 0.1, max: 1.0, step: 0.01 },
       },
