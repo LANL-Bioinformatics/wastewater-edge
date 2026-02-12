de95d5824b2f75ecec66fbf5d8caa06fe6833dab
Yan Xu <yxu@pn2405079.lanl.gov>
2026-02-11 22:29:50 -0700

    Handle err in nextflowMonitors

--- webapp/server/workflow/util.js
+++ webapp/server/workflow/util.js
@@ -55,7 +55,6 @@ const workflowList = {
 // eslint-disable-next-line no-unused-vars
 const generateNextflowWorkflowParams = async (projHome, projectConf, proj) => {
   const params = {}
-  const errMsg = ''
   if (projectConf.workflow.name === 'sra2fastq') {
     // download sra data to shared directory
     params.sraOutdir = config.IO.SRA_BASE_DIR
@@ -107,7 +106,7 @@ const generateNextflowWorkflowParams = async (projHome, projectConf, proj) => {
     csvStream.end()
     params.metadata = csvFile
   }
-  return { workflowParams: errMsg ? null : params, errMsg }
+  return params
 }
 
 const generateWorkflowResult = proj => {
